c17a04ab7b0dd43ea66f358395dbe1e03f37d180
Spectari-code <SpectariVoid@gmail.com>
2021-10-31 15:41:01 +0200

    Animations and basic movement the animated sprite

--- Action RPG/Player/Player.gd
+++ Action RPG/Player/Player.gd
@@ -6,6 +6,17 @@ const FRICTION = 500
 
 var velocity = Vector2.ZERO
 
+# ref to the path of the node AnimationPlayer
+#var animationPlayer = null
+
+#func _ready():
+	# ref is set equal to the path of the node
+#	animationPlayer = $AnimationPlayer
+
+# onready is better way to get te ref and set it equal to the path of the node
+# the ready function is therefore not needed anymore
+onready var animationPlayer = $AnimationPlayer
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -13,8 +24,13 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		if input_vector.x > 0:
+			animationPlayer.play("RunRight")
+		else:
+			animationPlayer.play("RunLeft")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animationPlayer.play("IdleRight")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	velocity = move_and_slide(velocity)
